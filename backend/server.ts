a1e4ea4f3901f582f66b9d3c2bc3d0929e13192f
LengerLim <lenger.lim@allweb.com.kh>
2022-10-07 15:51:31 +0700

    routes created

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,9 @@
-console.log('Hello Test');
+import * as express from 'express';
+import { PORT } from './utils/config';
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/projects', require('./routes/projectRoutes'));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
