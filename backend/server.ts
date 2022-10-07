5255e13798b7a1bf433a99ba7cb12512e380dd13
LengerLim <lenger.lim@allweb.com.kh>
2022-10-07 16:33:13 +0700

    created error handler

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,12 @@
-import * as express from 'express';
-import { PORT } from './utils/config';
+import * as express from "express";
+import { errorHandler } from "./middleware/errorMiddleware";
+import { PORT } from "./utils/config";
 
 const app = express();
 app.use(express.json());
 
-app.use('/api/projects', require('./routes/projectRoutes'));
+app.use("/api/projects", require("./routes/projectRoutes"));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
