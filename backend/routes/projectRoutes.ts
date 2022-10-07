70016161071e526201cb3f2d51fc36fbee4b177c
LengerLim <lenger.lim@allweb.com.kh>
2022-10-07 16:16:12 +0700

    created routes

--- backend/routes/projectRoutes.ts
+++ backend/routes/projectRoutes.ts
@@ -1,21 +1,15 @@
-const express = require('express');
-const routes = express.Router();
-import { Response, Request } from 'express';
+const express = require("express");
+const router = express.Router();
 
-routes.get('/', (req: Request, res: Response) => {
-    res.json({ message: 'Get All Projects' });
-});
-routes.post('/', (req: Request, res: Response) => {
-    res.json({ message: 'Create A Project' });
-});
-routes.get('/:id', (req: Request, res: Response) => {
-    res.json({ message: `Get Project ${req.params.id}` });
-});
-routes.put('/:id', (req: Request, res: Response) => {
-    res.json({ message: `Update Project ${req.params.id}` });
-});
-routes.delete('/:id', (req: Request, res: Response) => {
-    res.json({ message: `Delete Project ${req.params.id}` });
-});
+const {
+  getProjects,
+  createProject,
+  getProject,
+  updateProject,
+  deleteProject,
+} = require("../controllers/projectController");
 
-module.exports = routes;
+router.route("/").get(getProjects).post(createProject);
+router.route("/:id").get(getProject).put(updateProject).delete(deleteProject);
+
+module.exports = router;
